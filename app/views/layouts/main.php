3a31cc235cb8744bc244d48475df490fa1af4a3e
Antonio Ramirez <amigo.cobos@gmail.com>
2013-07-18 09:25:58 +0200

    added analytics

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -85,16 +85,12 @@
     _paq.push(["trackPageView"]);
     _paq.push(["enableLinkTracking"]);
 
-    (function () {
-        var u = (("https:" == document.location.protocol) ? "https" : "http") + "://analytics.2amigos.us/";
-        _paq.push(["setTrackerUrl", u + "piwik.php"]);
-        _paq.push(["setSiteId", "3"]);
-        var d = document, g = d.createElement("script"), s = d.getElementsByTagName("script")[0];
-        g.type = "text/javascript";
-        g.defer = true;
-        g.async = true;
-        g.src = u + "piwik.js";
-        s.parentNode.insertBefore(g, s);
+    (function() {
+        var u=(("https:" == document.location.protocol) ? "https" : "http") + "://analytics.2amigos.us/";
+        _paq.push(["setTrackerUrl", u+"piwik.php"]);
+        _paq.push(["setSiteId", "4"]);
+        var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+        g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
     })();
 </script>
 <!-- End Piwik Code -->
