6f0e6689aca483e9a3c31a9a3e171df7b4b50748
Antonio Ramirez <amigo.cobos@gmail.com>
2014-09-08 14:33:13 +0200

    added disqus widget

--- app/views/site/inputs.php
+++ app/views/site/inputs.php
@@ -1244,7 +1244,21 @@ $this-&gt;widget('yiiwheels.widgets.select2.WhSelect2', array(
 ));?&gt;
     </pre>
 </section>
+<div id="disqus_thread"></div>
+<script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_shortname = 'yiiwheels'; // required: replace example with your forum shortname
+    var disqus_identifier = 'inputs';
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+</div>
+</div>
 
-</div>
-</div>
 
